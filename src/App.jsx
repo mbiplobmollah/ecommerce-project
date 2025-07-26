7ded66d3ede7dd604d8bb4f45b7d979506a4f78c
mbiplobmollah <biplobmollah16@gmail.com>
2025-07-26 23:54:37 +0600

    all needed folders are created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-2xl'>This is a text</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/allproducts/AllProduxts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllProduxts = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default AllProduxts;
