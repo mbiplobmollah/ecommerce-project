9b6a21e982b2f1757d531a8c8437500c0d159e7a
mbiplobmollah <biplobmollah16@gmail.com>
2025-07-27 23:47:54 +0600

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Route, Router, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Order from './pages/order/Order';
+import Cart from './pages/cart/Cart';
+import Dashboard from './pages/admin/dashboard/Dashboard';
+import NoPage from './pages/nopage/NoPage';
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-2xl'>This is a text</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/order' element={<Order></Order>}></Route>
+        <Route path='/cart' element={<Cart></Cart>}></Route>
+        <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+        <Route path='/*' element={<NoPage></NoPage>}></Route>
+      </Routes>
+    </Router>
   );
 };
 
