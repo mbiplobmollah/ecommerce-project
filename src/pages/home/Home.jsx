c8b2e18e212994c6a74a3dffbb2f47c3b7da2e20
mbiplobmollah <biplobmollah16@gmail.com>
2025-08-09 15:04:16 +0600

    procut card section is done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import Filter from '../../components/filter/Filter';
 import HeroSection from '../../components/heroSection/HeroSection';
 import Layout from '../../components/layout/Layout';
+import ProductCard from '../../components/productCard/ProductCard';
 import MyContext from '../../context/data/MyContext';
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
            <Layout>
             <HeroSection></HeroSection>
             <Filter></Filter>
+            <ProductCard></ProductCard>
            </Layout> 
     );
 };
