91f90871a2c0edfa690a8f0592100b0995ada555
mbiplobmollah <biplobmollah16@gmail.com>
2025-08-09 15:08:31 +0600

    track and testimonial section is done

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import Filter from '../../components/filter/Filter';
 import HeroSection from '../../components/heroSection/HeroSection';
 import Layout from '../../components/layout/Layout';
 import ProductCard from '../../components/productCard/ProductCard';
+import Testimonial from '../../components/testimonial/Testimonial';
+import Track from '../../components/track/Track';
 import MyContext from '../../context/data/MyContext';
 
 const Home = () => {
@@ -11,6 +13,8 @@ const Home = () => {
             <HeroSection></HeroSection>
             <Filter></Filter>
             <ProductCard></ProductCard>
+            <Track></Track>
+            <Testimonial></Testimonial>
            </Layout> 
     );
 };
